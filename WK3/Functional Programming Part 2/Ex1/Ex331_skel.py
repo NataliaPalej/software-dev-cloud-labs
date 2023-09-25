b7e5d98cd24afbffc93cace4eec2425fbc6a691c
Natalia Palej <palejnatalia@gmail.com>
2023-09-25 15:54:19 +0100

    HOF and List Processing example

---
+++ WK3/Functional Programming Part 2/Ex1/Ex331_skel.py
@@ -0,0 +1,23 @@
+def main():
+    list1 = [2, 3, 4, 5, 6, 7, 8, 9]
+    print("The original list is : " + str(list1))
+    result1 = hAdd(pDecr, list1.copy())
+    print('Add Items after Decrement=', result1)
+    result2 = hAdd(pStepEven, list1.copy())
+    print('Add Items after Stepping Even Items=', result2)
+
+
+pDecr = lambda a: a - 1
+
+pStepEven = lambda a: (a + 1 if (a % 2 == 0) else a)
+
+
+def hAdd(f, list1):
+    if len(list1) == 0:
+        return 0
+    else:
+        first = list1.pop(0)
+        result = f(first) + hAdd(f, list1)
+        return result
+
+main()
