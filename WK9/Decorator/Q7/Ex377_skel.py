ef1591b4ae6d27b1603d3694903b7ef5ab358ebd
Natalia <PalejNatalia@gmail.com>
2023-11-06 12:00:32 +0000

    Decorator repeat specified amount of times

---
+++ WK9/Decorator/Q7/Ex377_skel.py
@@ -0,0 +1,25 @@
+def main():
+    value=input('Enter Name:' )
+    printName(value)
+
+def repeat_void_decorator(func):
+    def closure(value):
+        for el in range(0,2):
+            func(value)
+    return closure
+
+def repeat_void(num_times=2):
+    def repeat_void_decorator(func):
+        def closure(value):
+            for i in range(0, num_times):
+                func(value)
+        return closure
+    return repeat_void_decorator
+
+@repeat_void(5)
+def printName(value):
+    print("Name is: ", value)
+
+
+
+main()
