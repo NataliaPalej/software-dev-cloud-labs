fa660aa8118a4ec1e004bdb0076a5196ebdfb45f
Natalia <PalejNatalia@gmail.com>
2023-11-06 16:00:09 +0000

    Build in decorator example - @staticmethod

---
+++ WK9/Build-In Decorator/Q1/EX381_skel.py
@@ -0,0 +1,43 @@
+class Account():
+   __accounts = 0
+
+   def __init__(self, name,balance):
+      self.__name=name
+      self.__balance=balance
+      self.__accounts = 0
+      Account.__accounts +=1
+      self.__number= 2000 + Account.__accounts
+
+   def deposit(self, amt):
+      self.balance += amt
+
+   def withdraw(self, amt):
+      self.balance -= amt
+
+   def printDetails(self):
+      print('\nAccount: {0}'.format(self.__name))
+      print('Balance: {0}'.format(self.__balance))
+
+   @staticmethod
+   def readNumberOfAccounts():
+      return Account.__accounts
+
+   def readNumber(self):
+      return self.__number
+
+
+def main():
+   list = [Account('JB', 500), Account('MJ', 200),Account('PJ', 1000)]
+   for el in list:
+      el.printDetails()
+      print('Number: {0}'.format(el.readNumber()))
+      # print('Number of Accounts: {0}'.format(Account.readNumberOfAccounts()))
+
+   newel=Account('MP',200)
+   newel.printDetails()
+   print('Number: {0}'.format(newel.readNumber()))
+   print('Number of Accounts: {0}'.format(Account.readNumberOfAccounts()))
+
+
+
+main()
---
+++ WK9/Ex379_skel.py
@@ -0,0 +1,23 @@
+def main():
+    printNames("John", "Joseph",  "Lennon")
+    print()
+    printNames("John", "Joseph","Michael", "Smith")
+    print()
+    printNames("John", "Joseph","Michael","Leo", "Jones")
+
+def max_args_decorator(func):
+    def closure(*args):
+        if (len(args)>4):
+            print("Only 4 Aruments Allowed")
+        else:
+            func(*args)
+    return closure
+
+@max_args_decorator
+def printNames(*args):
+    for el in args:
+        print(el,end=' ')
+
+
+
+main()
